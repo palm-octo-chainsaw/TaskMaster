57200d3f4187e8af707debde4b34d57412c0a6b3
Kristiyan <kristiyan.georgiev.bs@gmail.com>
2020-09-21 15:38:27 +0300

    Added Update and Delete functions

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask import request as req
 from flask import redirect as red
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask('__name__')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -11,7 +11,7 @@ db = SQLAlchemy(app)
 
 class ToDo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=True)
+    content = db.Column(db.String(200), nullable=False)
     date_create = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -26,12 +26,39 @@ def index():
         try:
             db.session.add(task)
             db.session.commit()
-            return red('/')
+            return red('/'), 201
         except:
-            return 'There was an issue with your request.'
+            return red('/'), 400
     else:
         tasks = ToDo.query.order_by(ToDo.date_create).all()
-        return render_template('index.html', tasks=tasks)
+        return render_template('index.html', tasks=tasks), 200
+
+@app.route('/delete/<int:id>/', methods=['GET', 'POST'])
+def delete(id):
+    delete_task = ToDo.query.get_or_404(id)
+
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        
+        return red('/'), 202
+    except:
+        return red('/'), 400
+
+@app.route('/update/<int:id>/', methods=['GET', 'POST'])
+def update(id):
+    task = ToDo.query.get_or_404(id)
+
+    if req.method == 'POST':
+        task.content = req.form['content']
+
+        try:
+            db.session.commit()
+            return red('/'), 200
+        except:
+            return red('/'), 400
+    else:
+        return render_template('update.html', task=task), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
